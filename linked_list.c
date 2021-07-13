52b458aebdd3e814b4e69fe863b38eeedb42c747
Dimo Dimchev <ddimchev7@gmail.com>
2021-07-13 13:46:45 +0300

    Implemented linked list solution with two methods: Insert -> insert element at the beginning of the list Print -> print all of the elements in the list

--- linked_list.c
+++ linked_list.c
@@ -1,6 +1,43 @@
+#include<stdlib.h>
+#include<stdio.h>
+
 struct Node{
-    // hold the data in the node
-    int data;
-    // hold the pointer
-    struct Node * link;
+    int data; // hold the data in the node 
+    struct Node* next; // pointer to the node 
 };
+
+struct Node* head;
+
+void Insert(int x){
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node)); // create a new node
+    temp->data = x; // asign x as the node's data
+    temp->next = head; // link the element to the next element in the list or NULL if the element is the only element in the list
+    head = temp; // make the current element's address the head node(beginning of list)
+}
+
+void Print(){
+    struct Node* temp = head;
+    printf("List is: ");
+    while(temp != NULL){ // until there are untraversed elements in the list
+        printf(" %d", temp->data); // print the element
+        temp = temp->next; // move on to the next element
+    }
+    printf("\n");
+}
+
+int main(){
+    head = NULL; // empty list
+
+    printf("How many numbers will the list consist of?\n");
+    int n,i, x;
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++ ){
+        printf("Enter the number for index %d\n", i);
+        scanf("%d", &x);
+        Insert(x);
+        Print();
+    }
+
+    return 0;
+}
