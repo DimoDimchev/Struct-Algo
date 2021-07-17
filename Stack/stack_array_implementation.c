1ab9dde9f52d441c5eb69b5126fcc45ce467bb78
Dimo Dimchev <ddimchev7@gmail.com>
2021-07-17 12:47:53 +0300

    implemented stack using array

---
+++ Stack/stack_array_implementation.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#define MAX_SIZE 101
+
+int A[MAX_SIZE]; // array to hold the stack
+int top = -1; // empty stack
+
+void Push(int x){
+    if(top == MAX_SIZE - 1){ // if push is called with no more space in the stack
+        printf("Error: stack overflow\n");
+        return;
+    }
+    
+    A[++top] = x; // move the top of the stack one index up
+}
+
+void Pop(){
+    if(top == -1){ // if there aren't any elements in the stack
+        printf("Error: no elements to pop\n");
+        return;
+    }
+    top--;
+}
+
+int Top(){
+    return A[top];
+} 
+
+void Print(){
+    int i;
+    printf("Stack: ");
+    for(i = 0; i <= top; i++){
+        printf("%d ", A[i]);
+    } 
+    printf("\n");
+}
+
+int main(){
+    Push(2);
+    Push(4);
+    Push(1);
+    Print();
+    printf("%d", Top());
+}
